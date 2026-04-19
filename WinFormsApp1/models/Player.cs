498ee7a329489e95a6029e579a602ce5db527740
olegp22 <oprotopopov924@gmail.com>
2026-04-19 22:09:57 +0500

    добавление функций к player

--- WinFormsApp1/models/Player.cs
+++ WinFormsApp1/models/Player.cs
@@ -1,10 +1,56 @@
-using System.Collections.Generic;
-using System.Linq;
-
 namespace Plunger;
 
 public class Sucker
 {
     public Point Location;
+    public int Speed;
+    public Condition Condition;
+    public Cannon Cannon;
+    public int CoinsCollected { get; private set; }
 
+    public Sucker(Point location, int speed, Condition condition, Cannon cannon = Cannon.Plunger)
+    {
+        Location = location;
+        Speed = speed;
+        Condition = condition;
+        Cannon = cannon;
+        CoinsCollected = 0;
+    }
+
+    public void AddCoin()
+    {
+        CoinsCollected++;
+    }
+
+    public void UpdatePosition(double deltaTime)
+    {
+        if (this.Condition == Condition.Run)
+            Location += new Point(Speed, 0);
+
+        else if (this.Condition == Condition.Attached)
+            Location += new Point(Speed, Speed / 2);//на приктике стоит определить лучшею формулу определения изменения по y
+
+        else if (this.Condition == Condition.Fall)
+            Location += new Point(Speed, -Speed / 2);
+
+        else if (this.Condition == Condition.ToStand)
+            Location += new Point(0,0);
+    }
+
+    public void Reset(Point startLocation)
+    {
+        Location = startLocation;
+        CoinsCollected = 0;
+        Condition = Condition.Death;
+    }
+
+    public Rectangle GetBounds(int w, int h)
+    {
+        // Передаем текущие координаты и размеры спрайта
+        return new Rectangle(Location.X, Location.Y, w, h);
+    }
+
+    public void Die() { this.Condition = Condition.Death; }
+
+    
 }
